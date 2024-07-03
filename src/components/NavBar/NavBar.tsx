8dba4f4c3a3ed6ec757d461b766d9c2f09818e5d
src1994 <sousamultimedia@gmail.com>
2024-07-03 22:55:32 +0100

    initial commit

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,42 @@
+// assim se importa images from svg
+import logoSVG from '../../assets/svg/logo.svg'
+import style from './NavBar.module.css'
+
+
+// 1 - criar função
+
+function NavBar() {
+
+    //2 - Obrigatorio ter um return
+    return (
+            <div className={style['main-header']}>
+                <div className={style['logo-wrapper']}><img src={logoSVG} alt="logo"/></div>
+                <nav className="links-wrapper">
+                    <div className="main-menu">
+                        <ul>
+                            <li>Home</li>
+                            <li>Shop</li>
+                            <li>About</li>
+                            <li>Blog</li>
+                            <li>Contacts</li>
+                            <li>Page</li>
+                        </ul>
+                    </div>
+                    <div className="utility-menu">
+                        <ul>
+                            <li><a href="login">Login</a></li>
+                            <li><a href="register">Register</a></li>
+                            <li><a href="">Search</a></li>
+                            <li><a href="">Cart</a></li>
+                            <li><a href="">Fav</a></li>
+                        </ul>
+                    </div>
+                </nav>
+               
+            </div>
+            )
+}
+
+
+//3 - Exportar componente(função)
+export default NavBar;
